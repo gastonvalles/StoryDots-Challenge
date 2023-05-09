b8e0dca9b6d081f6307bc45724521d58f8491e14
Gaston Valles <gastonvallesyeou@gmail.com>
2023-05-09 20:30:57 -0300

    Database deploy changes

--- api/src/db.js
+++ api/src/db.js
@@ -1,30 +1,63 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
-const fs = require('fs');
-const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+// const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { PG_USER, PG_PASSWORD, PG_HOST, PG_PORT, PG_DATABASE } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/storydots`, {
-  logging: false,
-  native: false,
-});
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/storydots`, {
+//   logging: false,
+//   native: false,
+// });
+
+const sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+        database: PG_DATABASE,
+        dialect: "postgres",
+        host: PG_HOST,
+        port: PG_PORT,
+        username: PG_USER,
+        password: PG_PASSWORD,
+        pool: {
+          max: 3,
+          min: 1,
+          idle: 10000,
+        },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+          keepAlive: true,
+        },
+        ssl: true,
+      })
+    : new Sequelize(
+        `postgres:${PG_USER}:${PG_PASSWORD}@${PG_HOST}/${PG_DATABASE}`,
+        { logging: false, native: false }
+      );
 
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
-fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+fs.readdirSync(path.join(__dirname, "/models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
 
-modelDefiners.forEach(model => model(sequelize));
+modelDefiners.forEach((model) => model(sequelize));
 
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+let capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
 sequelize.models = Object.fromEntries(capsEntries);
 
 const { Brand, Product } = sequelize.models;
@@ -32,7 +65,7 @@ const { Brand, Product } = sequelize.models;
 Brand.hasMany(Product);
 Product.belongsTo(Brand);
 
-const data = require('./data/data.js');
+const data = require("./data/data.js");
 
 async function loadData() {
   const { brandsJson, productsJson } = data;
@@ -55,5 +88,5 @@ async function loadData() {
 module.exports = {
   ...sequelize.models,
   conn: sequelize,
-  loadData
+  loadData,
 };
