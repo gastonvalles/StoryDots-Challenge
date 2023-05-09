8a75455a1899d418395cd746a5cb69d0cfadb8f5
Gaston Valles <gastonvallesyeou@gmail.com>
2023-05-09 20:40:46 -0300

    URI of actions changed to deploy

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,132 +1,159 @@
 import axios from "axios";
-import { DELETE_PRODUCT, GET_BRANDS, GET_BRANDS_BY_QUERY, GET_BRANDS_ID, GET_PRODUCTS, GET_PRODUCTS_BY_QUERY, GET_PRODUCTS_ID, UPDATE_PRODUCT } from "./constants";
+import {
+  DELETE_PRODUCT,
+  GET_BRANDS,
+  GET_BRANDS_BY_QUERY,
+  GET_BRANDS_ID,
+  GET_PRODUCTS,
+  GET_PRODUCTS_BY_QUERY,
+  GET_PRODUCTS_ID,
+  UPDATE_PRODUCT,
+} from "./constants";
 
 export function getAllBrands() {
-    return async (dispatch) => {
-        try {
-            let response = await axios.get("http://localhost:3001/brands");
-            return dispatch({
-                type: GET_BRANDS,
-                payload: response.data
-            })
-        } catch (error) {
-            console.log(error)
-        }
+  return async (dispatch) => {
+    try {
+      let response = await axios.get(
+        `https://storydots-challenge-production.up.railway.app/brands`
+      );
+      return dispatch({
+        type: GET_BRANDS,
+        payload: response.data,
+      });
+    } catch (error) {
+      console.log(error);
     }
+  };
 }
 
 export function getBrandsId(id) {
-    return async (dispatch) => {
-        try {
-            let response = await axios.get(`http://localhost:3001/brands/${id}`);
-            return dispatch({
-                type: GET_BRANDS_ID,
-                payload: response.data
-            })
-        } catch (error) {
-            console.log(error)
-        }
+  return async (dispatch) => {
+    try {
+      let response = await axios.get(
+        `https://storydots-challenge-production.up.railway.app/brands/${id}`
+      );
+      return dispatch({
+        type: GET_BRANDS_ID,
+        payload: response.data,
+      });
+    } catch (error) {
+      console.log(error);
     }
+  };
 }
 
 export function getBrandsByQuery(name) {
-    return async (dispatch) => {
-        try {
-            let response = await axios.get(`http://localhost:3001/brands?name=${name}`);
-            return dispatch({
-                type: GET_BRANDS_BY_QUERY,
-                payload: response.data
-            })
-        } catch (error) {
-            console.log(error)
-        }
+  return async (dispatch) => {
+    try {
+      let response = await axios.get(
+        `https://storydots-challenge-production.up.railway.app/brands?name=${name}`
+      );
+      return dispatch({
+        type: GET_BRANDS_BY_QUERY,
+        payload: response.data,
+      });
+    } catch (error) {
+      console.log(error);
     }
+  };
 }
 
 export function postBrand(payload) {
-    return async () => {
-        try {
-            let response = await axios.post("http://localhost:3001/products", payload);
-            return response
-        } catch (error) {
-            console.log(error)
-        }
+  return async () => {
+    try {
+      let response = await axios.post(
+        `https://storydots-challenge-production.up.railway.app/products`,
+        payload
+      );
+      return response;
+    } catch (error) {
+      console.log(error);
     }
+  };
 }
 
 export function getAllProducts() {
-    return async (dispatch) => {
-        try {
-            let response = await axios.get("http://localhost:3001/products");
-            return dispatch({
-                type: GET_PRODUCTS,
-                payload: response.data
-            })
-        } catch (error) {
-            console.log(error)
-        }
+  return async (dispatch) => {
+    try {
+      let response = await axios.get(
+        `https://storydots-challenge-production.up.railway.app/products`
+      );
+      return dispatch({
+        type: GET_PRODUCTS,
+        payload: response.data,
+      });
+    } catch (error) {
+      console.log(error);
     }
+  };
 }
 
 export function getProductsId(id) {
-    return async (dispatch) => {
-        try {
-            let response = await axios.get(`http://localhost:3001/products/${id}`);
-            return dispatch({
-                type: GET_PRODUCTS_ID,
-                payload: response.data
-            })
-        } catch (error) {
-            console.log(error)
-        }
+  return async (dispatch) => {
+    try {
+      let response = await axios.get(
+        `https://storydots-challenge-production.up.railway.app/products/${id}`
+      );
+      return dispatch({
+        type: GET_PRODUCTS_ID,
+        payload: response.data,
+      });
+    } catch (error) {
+      console.log(error);
     }
+  };
 }
 
 export function getProductsByQuery(name) {
-    return async (dispatch) => {
-        try {
-            let response = await axios.get(`http://localhost:3001/products?name=${name}`);
-            return dispatch({
-                type: GET_PRODUCTS_BY_QUERY,
-                payload: response.data
-            })
-        } catch (error) {
-            console.log(error)
-        }
+  return async (dispatch) => {
+    try {
+      let response = await axios.get(
+        `https://storydots-challenge-production.up.railway.app/products?name=${name}`
+      );
+      return dispatch({
+        type: GET_PRODUCTS_BY_QUERY,
+        payload: response.data,
+      });
+    } catch (error) {
+      console.log(error);
     }
+  };
 }
 
 export function postProduct(payload) {
-    return async () => {
-        try {
-            let response = await axios.post("http://localhost:3001/products", payload);
-            return response
-        } catch (error) {
-            console.log(error)
-        }
+  return async () => {
+    try {
+      let response = await axios.post(
+        `https://storydots-challenge-production.up.railway.app/products`,
+        payload
+      );
+      return response;
+    } catch (error) {
+      console.log(error);
     }
+  };
 }
 
-export function updateProduct(product){
-    return {
-        type: UPDATE_PRODUCT,
-        payload: product
-    }
+export function updateProduct(product) {
+  return {
+    type: UPDATE_PRODUCT,
+    payload: product,
+  };
 }
 
 export function deleteProduct(id) {
-    return async (dispatch) => {
-        try {
-            let response = await axios.delete(`http://localhost:3001/products/${id}`);
-            console.log(response)
-            return dispatch({
-                type: DELETE_PRODUCT,
-                payload: id,
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  return async (dispatch) => {
+    try {
+      let response = await axios.delete(
+        `https://storydots-challenge-production.up.railway.app/products/${id}`
+      );
+      console.log(response);
+      return dispatch({
+        type: DELETE_PRODUCT,
+        payload: id,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
-
-
