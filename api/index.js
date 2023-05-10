129c2f8b28884b25b7811defaae0af351402df4a
Gaston Valles <gastonvallesyeou@gmail.com>
2023-05-09 21:10:18 -0300

    Reset

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const server = require("./src/app.js");
 const { conn, loadData } = require("./src/db.js");
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   loadData();
   server.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
