7ab41c9ca09bf977547563cf7edcbfa28f1f6969
Gaston Valles <gastonvallesyeou@gmail.com>
2023-05-09 21:06:24 -0300

    test 1

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-const server = require('./src/app.js');
-const { conn, loadData } = require('./src/db.js');
+const server = require("./src/app.js");
+const { conn, loadData } = require("./src/db.js");
 
-conn.sync({ force: true }).then(() => {
-    loadData();
-    server.listen(process.env.PORT, () => {
-        console.log(`Server running on port ${process.env.PORT}`);
-    });
+conn.sync({ force: false }).then(() => {
+  loadData();
+  server.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
 });
